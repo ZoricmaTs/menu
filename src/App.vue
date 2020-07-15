55965f9f9ccab1c2f386f6bcd900f8c2cec76c00
ZoricmaTs <tszzzch@gmail.com>
2020-07-15 20:13:21 +0700

    added fonts, Content

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,137 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div>
+    <nav role="navigation">
+      <div id="menuToggle">
+        <input type="checkbox" />
+
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul class="nav__list" id="menu">
+          <li class="nav__item" v-for="item in menu" :key="item.id">
+            <a class="nav__link" href="#">
+              <div class="nav__title">
+                {{item.menu_title}}
+                <p class="nav__subtitle">{{item.menu_subtitle}} &nbsp;</p>
+              </div>
+              <div class="nav__arrow material-icons">navigate_next</div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <section class="body-block">
+      <div class="screen-size">
+        <div class="screen-size__block">
+          <span class="screen-size__vers material-icons">stay_current_portrait</span>
+          <span class="screen-size__vers material-icons">tablet_mac</span>
+          <span class="screen-size__vers material-icons">tv</span>
+        </div>
+      </div>
+      <div class="screen-size__line"></div>
+      <Content :menu="this.menu_title[this.active]" />
+    </section>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+@import "@/assets/scss/style.scss";
+* {
+  margin: 0;
+  padding: 0;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  font-family: "Raleway", sans-serif;
+  font-style: normal;
+  font-weight: 400;
 }
 </style>
+
+<script>
+import Content from "@/components/Content.vue";
+export default {
+  components: {
+    Content
+  },
+  data() {
+    return {
+      menu: [
+        {
+          id: 1,
+          menu_title: "Main page",
+          menu_subtitle: "About us",
+          content: {
+            header: "Lorem ipsum",
+            short_text:
+              "Suspendisse fringilla dui nulla, sit amet fermentum nunc consectetur eget.",
+            full_text:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            image_url: "/path/to/image/file.png",
+            full_text_btn_title: "Learn more"
+          }
+        },
+        {
+          id: 2,
+          menu_title: "Shop",
+          menu_subtitle: "Our products to buy",
+          content: {
+            header: "Products",
+            short_text:
+              "Suspendisse fringilla dui nulla, sit amet fermentum nunc consectetur eget. In vitae nulla a est tristique porttitor.",
+            full_text:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            image_url: "/path/to/image/file2.png",
+            full_text_btn_title: "Get more products"
+          }
+        },
+        {
+          id: 3,
+          menu_title: "Contacts",
+          menu_subtitle: "Call us",
+          content: {
+            header: "Contacts",
+            short_text: "Phone:  8-800-555-3555 \nEmail: some_email@domain.com",
+            full_text: "",
+            image_url: "/path/to/image/file3.png",
+            full_text_btn_title: "Learn more"
+          }
+        },
+        {
+          id: 4,
+          menu_title: "Thank you",
+          menu_subtitle: "",
+          content: {
+            header: "Thank you",
+            short_text: "We will be glad to see you again",
+            full_text:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            image_url: "",
+            full_text_btn_title: ""
+          }
+        },
+        {
+          id: 4,
+          menu_title: "Thank you",
+          menu_subtitle: "",
+          content: {}
+        }
+      ]
+    };
+  },
+  methods: {
+    change(index) {
+      this.invert = index < this.active;
+      this.active = (index + this.total) % this.total;
+    }
+  },
+  mounted() {},
+  computed: {
+    total() {
+      return this.menu.length;
+    }
+  }
+};
+</script>
---
+++ src/components/Content.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="content">
+    <!-- <img class="content__img" src="./img/file.jpg" /> -->
+    <div class="content__block-text">
+      <header class="content__header">Drag and drop website builder</header>
+      <p class="content__short-text">Make you own website in a few clicks!fdgdfgdfgdg</p>
+      <p class="content__full-text">&nbsp;</p>
+      <button class="content__btn">learn more</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
